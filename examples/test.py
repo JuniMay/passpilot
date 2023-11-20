78a5f6bc9fe3b3df5a0572fd611bebf66a6d5b88
LeeOrange-is-me <1254216281@qq.com>
2023-11-20 11:04:16 +0800

    fix(toml):fix actions

--- examples/test.py
+++ examples/test.py
@@ -10,11 +10,12 @@ argparser.add_argument("-c", "--config", help="config file path")
 argparser.add_argument("-k", "--api-key", help="api key for openai")
 
 args = argparser.parse_args()
-print(args.api_key,os.getenv("OPENAI_API_KEY"))
+
 if args.api_key!=None:
     openai.api_key = args.api_key
 else:
     openai.api_key=os.getenv("OPENAI_API_KEY")
+print(args.api_key,os.getenv("OPENAI_API_KEY"))
 
 
 
